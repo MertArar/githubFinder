a18885c39e012d19af93e641389cbde931c6accc
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-18 02:22:53 +0300

    seaction component added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import GithubState from "./Context/Github/GithubState";
 
 import Navbar from "./components/Layout/Navbar";
+import Seaction from "./components/Layout/Seaction";
 
 const App = () => {
   return (
     <GithubState>
       <BrowserRouter>
         <Navbar />
+        <Seaction />
       </BrowserRouter>
     </GithubState>
   );
---
+++ src/components/Layout/Users/Useritem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Useritem = () => {
+  return <div>Useritem</div>;
+};
+
+export default Useritem;
