e3337ffa3356bc76de52fc0b51e675aef56bd695
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-18 05:49:40 +0300

    pages configurade

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GithubState from "./Context/Github/GithubState";
+
+//COMPONENTS
+
 import Navbar from "./components/Layout/Navbar";
 import Users from "./components/Layout/Users/Users";
+import About from "./components/Pages/About";
 
 const App = () => {
   return (
@@ -11,6 +15,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Users />} />
+          <Route exact path="/" element={<About />} />
         </Routes>
       </BrowserRouter>
     </GithubState>
